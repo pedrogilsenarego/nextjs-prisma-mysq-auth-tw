f64d92303eb0a1130e169d37f3ec611a5074e27c
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-07-21 16:55:27 +0100

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,25 @@
-import '@/styles/globals.css'
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn("bg-white text-slate-900 antialias light", inter.className)}
+    >
+      <body className="min-h-screen">{children}</body>
     </html>
-  )
+  );
 }
