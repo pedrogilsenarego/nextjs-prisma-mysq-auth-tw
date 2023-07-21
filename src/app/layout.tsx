f539f925efcb4734b650374ab1c967a887a1e67b
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-07-21 17:17:39 +0100

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,16 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("bg-white text-slate-900 antialias light", inter.className)}
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
     >
-      <body className="min-h-screen">{children}</body>
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
