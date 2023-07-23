bab0255088874094a73a43ef99c071d3db9c437e
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-07-23 12:01:15 +0100

    a

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -17,4 +17,17 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_SECRET_ID!,
     }),
   ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+        session.user.username = token.username;
+      }
+
+      return session;
+    },
+  },
 };
