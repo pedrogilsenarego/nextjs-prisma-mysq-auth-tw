1faefeb1b3ae18cf2ce937d07f34ce268e6bce1c
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-07-29 10:08:04 +0100

    a

---
+++ src/app/r/create/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
+const Page = () => {
+  const [input, setInput] = useState<string>("");
+  return <div className="container flex items-center h-full"></div>;
+};
+
+export default Page;
